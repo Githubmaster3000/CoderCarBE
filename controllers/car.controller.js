42a626863d07cb12665cc0df42d20f314800bd6f
khanh <ngiakhanh18@gmail.com>
2024-02-07 08:17:20 -0600

    commit

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,37 +1,125 @@
-const mongoose = require('mongoose');
-const Car = require('../models/Car');
+const mongoose = require("mongoose");
+const Car = require("../models/Car");
 const carController = {};
 
 carController.createCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  const { page } = req.body;
+  try {
+    const { make, model, release_date, transmission_type, size, style, price } =
+      req.body;
+    const car = await Car.create({
+      // might replace this with .save()
+      make,
+      model,
+      release_date,
+      transmission_type,
+      size,
+      style,
+      price,
+    });
+    // Update the response format
+    const response = {
+      message: "Create Car Successfully!",
+      car: {
+        make,
+        model,
+        release_date,
+        transmission_type,
+        size,
+        style,
+        price,
+      },
+    };
+    console.log(response);
+    res.json(response); // Send the response as JSON
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
 };
 
 carController.getCars = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    const page = parseInt(req.query.page) || 1; // default to page 1 if not specified
+    const limit = parseInt(req.query.limit) || 10; // default to 10 items per page if not specified
+    const skip = (page - 1) * limit;
+
+    const cars = await Car.find({ isDeleted: false }).skip(skip).limit(limit);
+
+    const totalCarsCount = await Car.countDocuments({ isDeleted: false });
+
+    const response = {
+      message: "Get Car List Successfully!",
+      cars: cars,
+      page: 1,
+      total: Math.ceil(totalCarsCount / limit),
+    };
+    console.log(cars);
+    res.json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
 };
 
 carController.editCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    const { make, model, release_date, transmission_type, size, style, price } =
+      req.body;
+    const id = req.params.id;
+
+    const car = await Car.findByIdAndUpdate(
+      id,
+      {
+        $set: {
+          make,
+          model,
+          release_date,
+          transmission_type,
+          size,
+          style,
+          price,
+        },
+      },
+      { new: true }
+    );
+
+    if (!car) {
+      return res.status(404).json({ message: "Car not found" });
+    }
+
+    // Update the response format
+    const response = {
+      message: "Update Car Successfully!",
+      car: car,
+    };
+
+    console.log(response);
+    res.json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 carController.deleteCar = async (req, res, next) => {
-	try {
-		// YOUR CODE HERE
-	} catch (err) {
-		// YOUR CODE HERE
-	}
+  try {
+    const { id } = req.params;
+    const car = await Car.findByIdAndUpdate(
+      id,
+      { $set: { isDeleted: true } },
+      { new: true }
+    );
+
+    const response = {
+      message: "Delete Car Successfully!",
+      car: car,
+    };
+    console.log(response);
+    res.json(response);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = carController;
--- models/Car.js
+++ models/Car.js
@@ -1,59 +1,86 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const carSchema = new mongoose.Schema(
-	{
-		make: {
-			type: String,
-			required: true,
-		},
-		model: {
-			type: String,
-			required: true,
-		},
-		release_date: {
-			type: Number,
-			min: 1900,
-			required: true,
-		},
-		transmission_type: {
-			type: String,
-			enum: ['MANUAL', 'AUTOMATIC', 'AUTOMATED_MANUAL', 'DIRECT_DRIVE', 'UNKNOWN'],
-			required: true,
-		},
-		size: {
-			type: String,
-			enum: ['Compact', 'Midsize', 'Large'],
-			required: true,
-		},
-		style: {
-			type: String,
-			required: true,
-		},
-		price: {
-			type: Number,
-			required: true,
-		},
-		isDeleted: { type: Boolean, default: false, required: true },
-	},
-	{
-		timestamps: true,
-	}
+  {
+    make: {
+      type: String,
+      required: true,
+    },
+    model: {
+      type: String,
+      required: true,
+    },
+    release_date: {
+      type: Number,
+      min: 1900,
+      required: true,
+    },
+    transmission_type: {
+      type: String,
+      enum: [
+        "MANUAL",
+        "AUTOMATIC",
+        "AUTOMATED_MANUAL",
+        "DIRECT_DRIVE",
+        "UNKNOWN",
+      ],
+      required: true,
+    },
+    size: {
+      type: String,
+      enum: ["Compact", "Midsize", "Large"],
+      required: true,
+    },
+    style: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    isDeleted: { type: Boolean, default: false, required: true },
+  },
+  {
+    timestamps: true,
+  }
 );
 
+// carSchema.pre(/^find/, function (next) {
+// 	if (!('_conditions' in this)) return next();
+// 	if (!('isDeleted' in carSchema.paths)) {
+// 		delete this['_conditions']['all'];
+// 		return next();
+// 	}
+// 	if (!('all' in this['_conditions'])) {
+// 		//@ts-ignore
+// 		this['_conditions'].isDeleted = false;
+// 	} else {
+// 		delete this['_conditions']['all'];
+// 	}
+// 	next();
+// });
+
 carSchema.pre(/^find/, function (next) {
-	if (!('_conditions' in this)) return next();
-	if (!('isDeleted' in carSchema.paths)) {
-		delete this['_conditions']['all'];
-		return next();
-	}
-	if (!('all' in this['_conditions'])) {
-		//@ts-ignore
-		this['_conditions'].isDeleted = false;
-	} else {
-		delete this['_conditions']['all'];
-	}
-	next();
+  // Check if 'isDeleted' field exists in the schema
+  if (!("isDeleted" in carSchema.paths)) {
+    // If 'isDeleted' field doesn't exist, remove 'all' condition and proceed
+    if ("all" in this._conditions) {
+      delete this._conditions.all;
+    }
+    return next();
+  }
+
+  // If 'all' condition is not present, add 'isDeleted' condition
+  if (!("all" in this._conditions)) {
+    this._conditions.isDeleted = false;
+  } else {
+    // If 'all' condition is present, remove it and proceed
+    delete this._conditions.all;
+  }
+
+  next();
 });
 
-const Car = mongoose.model('Car', carSchema);
+const Car = mongoose.model("Car", carSchema);
 
 module.exports = Car;
